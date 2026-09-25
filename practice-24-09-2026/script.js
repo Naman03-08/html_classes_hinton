7b00b6d29ee51982f0eb53685ca420f000b0c2ef
Naman03-08 <naman03mgs@gmail.com>
2026-09-25 12:41:06 +0530

    created chese board from start to end

--- practice-24-09-2026/script.js
+++ practice-24-09-2026/script.js
@@ -1,14 +1,14 @@
-num = prompt("enter any score: ")
+// num = prompt("enter any score: ")
 
-if (80 <= num <= 100){
-    console.log("you got A grade with marks:", num)
-}
-else if(60 <= num <= 80){
-    console.log("you got grade B with mark:", num)
-}
-else if(40 <= num <= 60 ){
-    console.log("you got C grade with marks:", num)
-}
-else{
-    console.log("you grade is D with marks:", num)
-}
+// if (80 <= num <= 100){
+//     console.log("you got A grade with marks:", num)
+// }
+// else if(60 <= num <= 80){
+//     console.log("you got grade B with mark:", num)
+// }
+// else if(40 <= num <= 60 ){
+//     console.log("you got C grade with marks:", num)
+// }
+// else{
+//     console.log("you grade is D with marks:", num)
+// }
