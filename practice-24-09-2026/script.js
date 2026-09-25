7112689289afad57e1ac64c11953795f71cf720e
Naman03-08 <naman03mgs@gmail.com>
2026-09-25 10:23:41 +0530

    new js code added

--- practice-24-09-2026/script.js
+++ practice-24-09-2026/script.js
@@ -1,2 +1,13 @@
-a = "new"
-console.log(typeof(a))
+num = prompt("enter any score: ")
+if (100 <= num <= 80){
+    console.log("you got A grade with marks:", num)
+}
+else if(80 <= num <= 60){
+    console.log("you got grade B with mark:", num)
+}
+else if( 60<= num <= 40){
+    console.log("you got C grade with marks:", num)
+}
+else if(num > 40){
+    console.log("you grade is D with marks:", num)
+}
